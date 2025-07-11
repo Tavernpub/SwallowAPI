6f7bcde6f28bcf389d6b11dcca4b36e6f8023288
Tavernpub <tavernpub@yeah.net>
2025-07-11 19:42:30 +0800

    Update 23.system_config.sql

--- database/23.system_config.sql
+++ database/23.system_config.sql
@@ -1,10 +1,11 @@
-INSERT INTO system_config (config_key, config_value, description)
+REPLACE INTO system_config (config_key, config_value, description)
 VALUES ('wall_content_audit', 'on', '内容发布是否需要审核，on为需要，off为无需审核');
 
-INSERT INTO system_config (config_key, config_value, description) VALUES ('file_upload_url', 'https://file.swallowpro.top/', '文件上传服务器地址');
+REPLACE INTO system_config (config_key, config_value, description)
+VALUES ('file_upload_url', 'https://file.swallowpro.top/', '文件上传服务器地址');
 
 -- 插入邮件配置信息
-INSERT INTO system_config (config_key, config_value, description) VALUES
+REPLACE INTO system_config (config_key, config_value, description) VALUES
 ('mail_smtp', 'smtp.qq.com', 'SMTP服务器地址'),
 ('mail_account', '2196008384@qq.com', '邮箱账户'),
 ('mail_password', 'ysnifqwjeyjtebca', '邮箱授权码'),
