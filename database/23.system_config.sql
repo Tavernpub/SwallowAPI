2ba5e5d14e795a2a67c9ca71d9b9ae1b30c1b458
Tavernpub <tavernpub@yeah.net>
2025-07-11 19:31:23 +0800

    Update 23.system_config.sql

--- database/23.system_config.sql
+++ database/23.system_config.sql
@@ -1,8 +1,7 @@
 INSERT INTO system_config (config_key, config_value, description)
 VALUES ('wall_content_audit', 'on', '内容发布是否需要审核，on为需要，off为无需审核');
 
-INSERT INTO system_config (config_key, config_value, description) VALUES ('file_upload_url', 'https://file.swallowpro.top/', '文件上传服务器地址')
-
+INSERT INTO system_config (config_key, config_value, description) VALUES ('file_upload_url', 'https://file.swallowpro.top/', '文件上传服务器地址');
 
 -- 插入邮件配置信息
 INSERT INTO system_config (config_key, config_value, description) VALUES
